27dbae5ed65f7c0ad81d3292adc11f130238e354
jeff9912 <183435906+jeff-A7X@users.noreply.github.com>
2025-04-14 19:52:32 +0200

    website update goated

--- javascript/members.js
+++ javascript/members.js
@@ -13,12 +13,19 @@ window.addEventListener("load", init);
 let memberNumber = 0;
 
 let memberPhotos = ['img/bandfoto2.png', 'img/memberPhoto1.jpeg', 'img/memberPhoto2.jpeg'];
-let memberNames = ['Alex Kokee ,', 'Jeff Stasis ', "Koen"];
-let memberInfo = ['lorem qjoibnqwuioebnqwiu ueiqwjeioqwue9ique98qu 89euwqehjoiqwje89-qwu e9wqhuiehuiahiou j9iafhuio',
-    'Lead Guitar & Backing Vocals ij2einqjne wniehawiehjiopjwaiop jokvm dkiaofj oiawje iofjaoif jioewaj fa', 'Drumsajhrui ewah ruiheuir hawiur hwiaohr ijewah riuowehaiur hawiurh ewuih ruiewah rui h',];
-let memberInstagram = ['https://www.instagram.com/alexkokee/', 'https://www.instagram.com/jeffstasis/', 'https://www.instagram.com/koenerkeland/'];
-let memberTwitter = ['https://twitter.com/alexkokee', 'https://twitter.com/jeffstasis', 'https://twitter.com/koenerkeland'];
+let memberNames = ['Alex Kokee', 'Jeff Stasis ', "Koen"];
+let memberFirstNames = ['Alex', 'Jeff', 'Koen'];
+let memberInfo = [  "I’m the lead vocalist, songwriter, and rhythm guitarist for Cardiostasis. I’ve been into loud rock music since I was 14. My influences range from heavy bands like (Acid Bath, Melvins, Death) to alt-rock (Pixies, Sonic Youth, Talking Heads), grunge (Nirvana, Alice In Chains, L7), and the punk and classic icons (Fugazi, Dead Kennedys, Bowie, The Beatles), and much more. That mix shapes my sound and my approach to songwriting.",
+  " I'm the lead guitarist and backing vocalist in Cardiostasis, I've always had a huge passion for loud raw heavy and melodic music. All my emotion and energy goes into crafting and enjoying music influenced greatly by bands like Bullet for My Valentine, Trivium, Parkway Drive and other heavy hitting emotional metal bands. Whether on stage, in the studio or even at home, I'm all about the balance of creating the most hard hitting, headbanging riffs and vocals along with producing the most touching and emotional music.",
+"ik ben koen lol poep"]
+let memberInstagram = ['https://www.instagram.com/alex_kokee/', 'https://www.instagram.com/jeff_stasis/', 'https://www.instagram.com/koen_meloen_010/'];
+let memberTwitter = ['https://x.com/ThemeParkVex', 'https://x.com/raredutch', 'https://x.com/RyanGosling'];
 let memberYoutube = ['https://www.youtube.com/@alexkokee', 'https://www.youtube.com/@jeffstasis', 'https://www.youtube.com/@koenerkeland'];
+let playlistSources = ['https://open.spotify.com/embed/playlist/1J9Q7Er9HPo3hxpc95DyJU?utm_source=generator', 'https://open.spotify.com/embed/playlist/1wUKJmb0YvyGoeq3aXJM2C?utm_source=generator', 'https://open.spotify.com/embed/playlist/12uEFYFJLpo42HbBrQUGfZ?utm_source=generator'];
+
+
+
+
 
 function init() {
     let memberPhotoRetrieve = document.getElementById('member_foto_retrieve');
@@ -26,17 +33,24 @@ function init() {
     let memberInfoRetrieve = document.getElementById('memberDetail');
     let memberInstagramRetrieve = document.getElementById('instagram');
     let memberTwitterRetrieve = document.getElementById('twitter');
-    let memberYoutubeReterieve = document.getElementById('youtube');
+    // let memberYoutubeReterieve = document.getElementById('youtube');
 
     memberPhotoRetrieve.src = memberPhotos[memberNumber];
     memberNameRetrieve.innerHTML = memberNames[memberNumber];
     memberInfoRetrieve.innerHTML = memberInfo[memberNumber];
     memberInstagramRetrieve.href = memberInstagram[memberNumber];
     memberTwitterRetrieve.href = memberTwitter[memberNumber];
-    memberYoutubeReterieve.href = memberYoutube[memberNumber];
+    // memberYoutubeReterieve.href = memberYoutube[memberNumber];
 
     console.log(memberPhotoRetrieve.src)
     addEventListener('click', changeData);
+    // meet the member section
+
+
+    let memberFirstNamesRetrieve = document.getElementById('firstName');
+    let memberPlaylistRetrieve = document.querySelector('iframe');
+    memberFirstNamesRetrieve = memberFirstNames[memberNumber];
+    memberPlaylistRetrieve.src = playlistSources[memberNumber];
 }
 
 
@@ -60,13 +74,22 @@ function changeData(data) {
     let memberInfoRetrieve = document.getElementById('memberDetail');
     let memberInstagramRetrieve = document.getElementById('instagram');
     let memberTwitterRetrieve = document.getElementById('twitter');
-    let memberYoutubeReterieve = document.getElementById('youtube');
+    // let memberYoutubeReterieve = document.getElementById('youtube');
 
     memberPhotoRetrieve.src = memberPhotos[memberNumber];
     memberNameRetrieve.innerHTML = memberNames[memberNumber];
     memberInfoRetrieve.innerHTML = memberInfo[memberNumber];
     memberInstagramRetrieve.href = memberInstagram[memberNumber];
     memberTwitterRetrieve.href = memberTwitter[memberNumber];
-    memberYoutubeReterieve.href = memberYoutube[memberNumber];
+    // memberYoutubeReterieve.href = memberYoutube[memberNumber];
+
+
+    
+    let memberFirstNamesRetrieve = document.getElementById('firstName');
+    let memberPlaylistRetrieve = document.querySelector('iframe');
+    memberFirstNamesRetrieve.innerHTML = memberFirstNames[memberNumber] + "'s favourites";
+
+    memberPlaylistRetrieve.src = playlistSources[memberNumber];
+    
 
 }
